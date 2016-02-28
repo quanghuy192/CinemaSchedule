32676b5f5630849630c5e51da768a3816e48f22d
yeuembubu <quanghuydang1902@gmail.com>
2016-02-29 02:16:42 +0700

    update background

--- app/src/main/java/vn/quanghuy/cinemaschedule/activity/activity/MovieDetail.java
+++ app/src/main/java/vn/quanghuy/cinemaschedule/activity/activity/MovieDetail.java
@@ -1,7 +1,19 @@
 package vn.quanghuy.cinemaschedule.activity.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import vn.quanghuy.cinemaschedule.activity.R;
+
 /**
  * Created by huy on 13/02/2016.
  */
-public class MovieDetail {
+public class MovieDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.film_detail);
+    }
 }
