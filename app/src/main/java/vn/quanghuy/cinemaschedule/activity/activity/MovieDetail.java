dc0852b1e5617e33c7ea40ae8c926d798cca7d8a
QuangHuy <quanghuydang1902@gmail.com>
2016-05-03 20:55:57 +0700

    update code

--- app/src/main/java/vn/quanghuy/cinemaschedule/activity/activity/MovieDetail.java
+++ app/src/main/java/vn/quanghuy/cinemaschedule/activity/activity/MovieDetail.java
@@ -3,6 +3,7 @@ package vn.quanghuy.cinemaschedule.activity.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import vn.quanghuy.cinemaschedule.activity.R;
 
@@ -11,9 +12,17 @@ import vn.quanghuy.cinemaschedule.activity.R;
  */
 public class MovieDetail extends AppCompatActivity {
 
+    private String detailLink ;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.film_detail);
+
+        // init
+        Bundle bundle = getIntent().getExtras();
+        detailLink = bundle.getString("Link");
+
+        Toast.makeText(this,detailLink,Toast.LENGTH_SHORT).show();
     }
 }
